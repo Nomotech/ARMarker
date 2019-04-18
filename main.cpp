d403bff10cb9810718db07fdb1fd1dbcab225c4b
Nomotech <nmtts.1126@gmail.com>
2019-04-19 01:23:20 +0900

    opencv demo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 
@@ -19,7 +22,6 @@ int main() {
 	//camera parameters
 	double fx = 525.0; //focal
 	double fy = 525.0; //focal
-
 	double cx = 319.5; //optical centre x
 	double cy = 239.5; //optical centre y
 
@@ -45,8 +47,19 @@ int main() {
 	int Height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 	fx = fy = (double)Width;
 
+	bool loopFlag = true;
+	bool binFlag = false;
+	bool edgeFlag = false;
+	bool polyFlag = false;
+	bool trampFlag = false;
+	bool labelFlag = false;
+	int xorNum = 0;
+	bool cubeFlag = false;
+
+
+
 	// esc ÇâüÇ∑Ç‹Ç≈
-	while (cv::waitKey(5) != 0x1b) {
+	while (loopFlag) {
 		cv::Mat frame;
 		cap >> frame;
 		// ÉLÉÉÉvÉ`ÉÉÇ≈Ç´ÇƒÇ¢Ç»ÇØÇÍÇŒèàóùÇîÚÇŒÇ∑
@@ -58,14 +71,17 @@ int main() {
 		cv::Mat grayImage, binImage;
 		cv::cvtColor(frame, grayImage, cv::COLOR_BGR2GRAY);
 		cv::threshold(grayImage, binImage, 128.0, 255.0, cv::THRESH_OTSU);
-		//cv::imshow("bin", binImage);
+		if (binFlag) cv::imshow("bin", binImage);
+		else cv::destroyWindow("bin");
 
 		// ó÷äsíäèo
-		std::vector< std::vector< cv::Point > > contours;
+		std::vector<std::vector< cv::Point >> contours;
 		cv::findContours(binImage, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 		// åüèoÇ≥ÇÍÇΩó÷äsê¸ÇÃï`âÊ
-		for (auto contour = contours.begin(); contour != contours.end(); contour++) {
-			cv::polylines(frame, *contour, true, cv::Scalar(0, 255, 0), 2);
+		if (edgeFlag) {
+			for (auto contour = contours.begin(); contour != contours.end(); contour++) {
+				cv::polylines(frame, *contour, true, cv::Scalar(0, 255, 0), 2);
+			}
 		}
 
 		// ó÷äsÇ™éläpå`Ç©ÇÃîªíË
@@ -92,7 +108,7 @@ int main() {
 				dstPoints[2] = { (float)cols,	(float)rows };
 				dstPoints[3] = { (float)cols,	0 };
 				cv::Point2f aprPoints[4]{ approx[0], approx[1], approx[2], approx[3] };
-				cv::polylines(frame, approx, true, cv::Scalar(255, 0, 0), 2);
+				if (polyFlag) cv::polylines(frame, approx, true, cv::Scalar(255, 0, 0), 2);
 
 				cv::Mat H = getPerspectiveTransform(aprPoints, dstPoints);
 				warpPerspective(binImage, dst, H, dst.size());
@@ -102,84 +118,89 @@ int main() {
 				int num = 0;
 				for (int i = 0; i < 13; i++) {
 					bitwise_xor(dst, tramp[i], img_xor);
-//					cv::imshow("xor", img_xor);
-//					std::cout << cv::mean(img_xor)[0] << std::endl;
 					float mean = cv::mean(img_xor)[0];
 					if (mean < score) {
 						score = mean;
 						num = i + 1;
 					}
 				}
-				std::cout << num << std::endl;
-				cv::putText(frame, std::to_string(num), (approx[0] + approx[2]) / 2 + cv::Point{-5, 5}, cv::FONT_HERSHEY_PLAIN, 6.0, cv::Scalar(0, 0, 255), 3);
-
-				// solve PnP
-				cv::Mat rvec(3, 1, cv::DataType<double>::type);
-				cv::Mat tvec(3, 1, cv::DataType<double>::type);
-
-				std::vector<cv::Point3f > objectPoints;
-				objectPoints.push_back(cv::Point3f(0, 0, 10));
-				objectPoints.push_back(cv::Point3f(0, (float)rows, 10));
-				objectPoints.push_back(cv::Point3f((float)cols, (float)rows, 10));
-				objectPoints.push_back(cv::Point3f((float)cols, 0, 10));
-
-				std::vector<cv::Point2f> imagePoints;
-				imagePoints.push_back((cv::Point2f)approx[0]);
-				imagePoints.push_back((cv::Point2f)approx[1]);
-				imagePoints.push_back((cv::Point2f)approx[2]);
-				imagePoints.push_back((cv::Point2f)approx[3]);
-
-				cv::solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec);
+				if (xorNum > 0) {
+					bitwise_xor(dst, tramp[xorNum - 1], img_xor);
+					cv::imshow("xor", img_xor);
+				} else {
+					cv::destroyWindow("xor");
+				}
 				
-				float h = 100.0f;
-				float w = (float)cols;
-				float l = (float)rows;
-				std::vector<cv::Point3f> cube;
-				cube.push_back(cv::Point3f(0, 0, 0));
-				cube.push_back(cv::Point3f(w, 0, 0));
-				cube.push_back(cv::Point3f(w, l, 0));
-				cube.push_back(cv::Point3f(0, l, 0));
-				cube.push_back(cv::Point3f(0, 0, -h));
-				cube.push_back(cv::Point3f(w, 0, -h));
-				cube.push_back(cv::Point3f(w, l, -h));
-				cube.push_back(cv::Point3f(0, l, -h));
+				if (score < 10) {
+					if (labelFlag) cv::putText(frame, std::to_string(num), (approx[0] + approx[2]) / 2 + cv::Point{ -5, 5 }, cv::FONT_HERSHEY_PLAIN, 6.0, cv::Scalar(0, 0, 255), 3);
+
+					// solve PnP
+					cv::Mat rvec(3, 1, cv::DataType<double>::type);
+					cv::Mat tvec(3, 1, cv::DataType<double>::type);
+
+					std::vector<cv::Point3f > objectPoints;
+					objectPoints.push_back(cv::Point3f(0, 0, 10));
+					objectPoints.push_back(cv::Point3f(0, (float)rows, 10));
+					objectPoints.push_back(cv::Point3f((float)cols, (float)rows, 10));
+					objectPoints.push_back(cv::Point3f((float)cols, 0, 10));
+
+					std::vector<cv::Point2f> imagePoints;
+					imagePoints.push_back((cv::Point2f)approx[0]);
+					imagePoints.push_back((cv::Point2f)approx[1]);
+					imagePoints.push_back((cv::Point2f)approx[2]);
+					imagePoints.push_back((cv::Point2f)approx[3]);
+
+					cv::solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec);
+
+					float h = 100.0f;
+					float w = (float)cols;
+					float l = (float)rows;
+					std::vector<cv::Point3f> cube;
+					cube.push_back(cv::Point3f(0, 0, 0));
+					cube.push_back(cv::Point3f(w, 0, 0));
+					cube.push_back(cv::Point3f(w, l, 0));
+					cube.push_back(cv::Point3f(0, l, 0));
+					cube.push_back(cv::Point3f(0, 0, -h));
+					cube.push_back(cv::Point3f(w, 0, -h));
+					cube.push_back(cv::Point3f(w, l, -h));
+					cube.push_back(cv::Point3f(0, l, -h));
 
 
-				std::vector<cv::Point2f> cube2D;
-				cv::projectPoints(cube, rvec, tvec, cameraMatrix, distCoeffs, cube2D);
-				cv::line(frame, cube2D[0], cube2D[1], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[1], cube2D[2], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[2], cube2D[3], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[3], cube2D[0], cv::Scalar(255, 255, 255), 1, 4);
+					if (cubeFlag) {
+						std::vector<cv::Point2f> cube2D;
+						cv::projectPoints(cube, rvec, tvec, cameraMatrix, distCoeffs, cube2D);
+						cv::line(frame, cube2D[0], cube2D[1], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[1], cube2D[2], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[2], cube2D[3], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[3], cube2D[0], cv::Scalar(255, 255, 255), 1, 4);
 
-				cv::line(frame, cube2D[4], cube2D[5], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[5], cube2D[6], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[6], cube2D[7], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[7], cube2D[4], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[4], cube2D[5], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[5], cube2D[6], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[6], cube2D[7], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[7], cube2D[4], cv::Scalar(255, 255, 255), 1, 4);
 
-				cv::line(frame, cube2D[0], cube2D[4], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[1], cube2D[5], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[2], cube2D[6], cv::Scalar(255, 255, 255), 1, 4);
-				cv::line(frame, cube2D[3], cube2D[7], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[0], cube2D[4], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[1], cube2D[5], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[2], cube2D[6], cv::Scalar(255, 255, 255), 1, 4);
+						cv::line(frame, cube2D[3], cube2D[7], cv::Scalar(255, 255, 255), 1, 4);
+					}
+				}
 
-				cv::circle(frame, cube2D[0], 3, cv::Scalar(0, 0, 255));
+				if (trampFlag) cv::imshow("dst", dst);
+				else cv::destroyWindow("dst");
 
-/*
-				std::cout << "objectPoints: " << objectPoints << std::endl;
-				std::cout << "approx: " << approx << std::endl;
-				std::cout << "cameraMatrix: " << cameraMatrix << std::endl;
-				std::cout << "distCoeffs: " << distCoeffs << std::endl;
-				std::cout << "rvec: " << rvec << std::endl;
-				std::cout << "tvec: " << tvec << std::endl;
-				std::cout << "H: " << H << std::endl;
-				*/
-				std::stringstream sst;
-				sst << "area : " << area;
-				cv::putText(frame, sst.str(), approx[0], cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0, 128, 0));
-				//cv::imshow("dst", dst);
-				if (cv::waitKey(1) == 's') {
-					cv::imwrite("./img/test.jpg", dst);
-					std::cout << "save" << std::endl;
+				switch (cv::waitKey(10)) {
+					case '1': binFlag = !binFlag; break;
+					case '2': edgeFlag = !edgeFlag; break;
+					case '3': polyFlag = !polyFlag; break;
+					case '4': trampFlag = !trampFlag; break;
+					case '5': xorNum = (xorNum + 1) % 14; break;
+					case '6': labelFlag = !labelFlag; break;
+					case '7': cubeFlag = !cubeFlag; break;
+					case 's': cv::imwrite("./img/test.jpg", frame); break;
+					case 0x1b:
+						loopFlag = false;
+						break;
 				}
 			}
 		}
